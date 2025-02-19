e17e0411c8c07c2eb9f62213e3e8cc003ca57d37
Antoinette Peterson <arpeterson55@gmail.com>
2025-02-18 23:02:20 -0500

    Initial Commit

---
+++ APWorksOut/APWorksOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  APWorksOutApp.swift
+//  APWorksOut
+//
+//  Created by Antoinette Peterson on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct APWorksOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
